7ba8551231d86b6c97bb9984c59d9cdcb0089314
muhammad.khadafi <yusuf.khadafi@pusilkom.com>
2022-11-06 14:00:14 +0700

    initial code

---
+++ src/main/java/com/apap/demo/springbootjwt/SpringbootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.apap.demo.springbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtApplication.class, args);
+	}
+
+}
